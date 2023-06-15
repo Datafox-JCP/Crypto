9d3b30d4cef6096067a7c399bd8fb0e9b9d3fff2
Datafox-JCP <datafox@outlook.com>
2023-06-15 13:18:23 -0600

    Initial Commit

---
+++ Crypto/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Crypto
+//
+//  Created by Juan Hernandez Pazos on 15/06/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by Juan Hernandez Pazos on 15/06/23.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
